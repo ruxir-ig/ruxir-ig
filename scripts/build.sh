d9ea4a025e4b9c081bcba2a5c68ce7ea2f352992
Ruchir Kalokhe <ruchirkalokhe@gmail.com>
2025-11-08 11:03:04 +0530

    repaired file access paths in scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 typst compile resume/main.typ
-python scripts/typst_to_md.py resume/main.typ
+python typst_to_md.py ../resume/main.typ
 git add resume/main.typ resume/main.pdf resume/main.md
 git commit -m "Updated Resume"
 git push
